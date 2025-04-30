da91f52d2e337c50320d2fdc7b422c6fb3bc9aa0
harshitraj230 <harshitraj230@gmail.com>
2025-04-30 15:15:49 +0530

    Removed Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-import { BrowserRouter, Routes, Route } from "react-router";
+// import { BrowserRouter, Routes, Route } from "react-router";
 
 
 function App() {
@@ -45,17 +45,18 @@ function App() {
     <>
   {/* <Navbar title="TextUtils" aboutText="About TextUtils"/>  */}
   {/* <Navbar/> */}
-  <BrowserRouter>
+  {/* <BrowserRouter> */}
   <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} /> 
   <Alert alert={alert}/>
   <div className="container my-3">
-  <Routes>
-    <Route exact path="about" element={<About/>}/>
-    <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}/>
-  </Routes>
+  {/* <Routes> */}
+    {/* <Route exact path="about" element={<About/>}/> */}
+    {/* <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}/> */}
+  {/* </Routes> */}
   {/* <About/> */}
+  <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
   </div>
-  </BrowserRouter>
+  {/* </BrowserRouter> */}
     </>
   );
 }
